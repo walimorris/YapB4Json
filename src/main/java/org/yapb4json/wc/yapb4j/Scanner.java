fbfc367f239f0f757f58a229c31c5a57841c7e6f
Wali Morris <walimmorris@gmail.com>
2024-01-15 15:06:53 -0800

    add methods for string and number literals

--- src/main/java/org/yapb4json/wc/yapb4j/Scanner.java
+++ src/main/java/org/yapb4json/wc/yapb4j/Scanner.java
@@ -7,7 +7,19 @@ import static org.yapb4json.wc.yapb4j.TokenType.*;
 
 /**
  * In the Scanner, the raw json code is stored as a simple string, and a {@link List}
- * is provided to fill up with tokens that'll be generated.
+ * is provided to fill up with tokens that'll be generated. What is <b>Lexical grammar</b>?
+ * Lexical grammar, in computer science, is a formal grammar defining the syntax of token.
+ * The program is written using characters that are defined by the lexical structure of the
+ * language used. The lexical grammar lays down the rules governing how a character
+ * sequence is divided up into subsequences of characters, each part representing an
+ * individual token. Are you thinking about regular expressions?
+ * <p>
+ * The Scanner class helps lay the rules for Yapb4j and the methods to consume tokens within
+ * the framework of its lexical grammar for our definition of JavaScript Object Notation (JSON).
+ *
+ * @see Yapb4j
+ * @see #scanToken()
+ * @see #scanTokens()
  */
 public class Scanner {
     private final String source;
@@ -64,17 +76,94 @@ public class Scanner {
     private void scanToken() {
         char c = nextToken();
         switch (c) {
-            case '{': addToken(RIGHT_BRACKET); break;
-            case '}': addToken(LEFT_BRACKET); break;
-            case ':': addToken(COLON); break;
-            case ',': addToken(COMMA); break;
-            case '"': addToken(QUOTATION); break;
+            case '{':
+                addToken(RIGHT_BRACKET);
+                break;
+            case '}':
+                addToken(LEFT_BRACKET);
+                break;
+            case ':':
+                addToken(COLON);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+
+            // ignore whitespace
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+
+            // dealing with string literals
+            case '"':
+                stringLiteral();
+                break;
 
             // let's report errors outside our parser's
             default:
-                Yapb4j.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    numberLiteral();
+                } else {
+                    Yapb4j.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    /**
+     * Consumes as many digits as found for the integer part of the literal. A
+     * fractional part is then scanned for (the decimal), followed by atleast
+     * one digit. We need to look ahead and past the decimal point to ensure
+     * we have a decimal after the "."
+     */
+    private void numberLiteral() {
+        while (isDigit(peek())) {
+            nextToken();
+
+            // look for fractional part.
+            if (peek() == '.' && isDigit(peekNext())) {
+                // consume the "."
+                nextToken();
+                while (isDigit(peek())) {
+                    nextToken();
+                }
+            }
+        }
+        addToken(NUMERIC, Double.parseDouble(source.substring(start, current)));
+
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * Consumes tokens until the closing quotations is consumed. This ends
+     * the string literal. An error is reported if the source is ended
+     * before the closing quotation is consumed.
+     */
+    private void stringLiteral() {
+        while (peek() != '"' && !scanIsAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+                nextToken();
+            }
+        }
+
+        if (scanIsAtEnd()) {
+            Yapb4j.error(line, "Invalid json string literal.");
+            return;
+        }
+        nextToken();
+
+        // trim surround quotations and produce the string literal value
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
@@ -86,6 +175,33 @@ public class Scanner {
         }
         current++;
         return true;
+    }
+
+    /**
+     * Similar to {@link Scanner#nextToken()} but doesn't consume the character.
+     * This is considered a look-a-head.
+     *
+     * @return boolean
+     */
+    private char peek() {
+        if (scanIsAtEnd()) {
+            return '\0';
+        }
+        return source.charAt(current);
+    }
+
+    /**
+     * This is a look-a-head, and more so a double look-a-head.
+     *
+     * @see #peek()
+     *
+     * @return char
+     */
+    private char peekNext() {
+        if (current + 1 >= source.length()) {
+            return '\0';
+        }
+        return source.charAt(current + 1);
     }
 
     /**
